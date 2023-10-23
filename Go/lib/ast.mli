cf9eedb9fa91fad33b6840a1ca2389c3e1398938
SurfaceYellowDuck <lukonenko.nikitka@mail.ru>
2023-10-23 13:13:40 +0300

    Add mini ast of golang

--- Go/lib/ast.mli
+++ Go/lib/ast.mli
@@ -12,4 +12,50 @@ type 'name t =
 
 (* Application [f g ] *)
 (** In type definition above the 3rd constructor is intentionally without documentation
-to test linter *)
+    to test linter *)
+
+type type' =
+  | StringType
+  | IntegerType
+  | BooleanType
+  | FuncType of signature
+
+and signature =
+  { args : arg list
+  ; ret : ret_typ
+  }
+
+and arg = string * type'
+
+and ret_typ =
+  | One of type'
+  | Void
+
+type constant =
+  | Int of int
+  | String of string
+  | Bool of bool
+
+and binaryOperation =
+  | Minus
+  | Asterisk
+  | Equal
+
+and expression =
+  | Const of constant
+  | Ident of string
+  | BinOp of expression * binaryOperation * expression (* (a < b) *)
+  | Call of expression * expression list
+
+and block = statements list
+
+and statements =
+  | Ifstmt of expression * block * block
+  | ReturnStatement of expression option
+  | StatementsBlock of block
+
+type topLevelDeclaration =
+  (* | FuncTopLevelDeclaration of string *)
+  | FuncTopLevelDeclaration of string * signature * block
+  | VarTopLevelDeclaration of string * expression
+  | Block of block
