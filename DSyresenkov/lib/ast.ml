ca9cd732488e955ecd69868e86bc11215a2d4403
Ilya Syresenkov <ilya.syresenkov.04@gmail.com>
2023-10-26 13:50:40 +0300

    fix: change licence contributors
    Signed-off-by: Ilya Syresenkov <ilya.syresenkov.04@gmail.com>

--- DSyresenkov/lib/ast.ml
+++ DSyresenkov/lib/ast.ml
@@ -1,4 +1,4 @@
-(** Copyright 2021-2023, Kakadu, Ilya Syresenkov and contributors *)
+(** Copyright 2021-2023, Ilya Syresenkov *)
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
--- DSyresenkov/lib/parser.mli
+++ DSyresenkov/lib/parser.mli
@@ -2,8 +2,6 @@
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
-(* let rec fac x = if x = 1 then x else x * (fac x) *)
-
 open Ast
 
 val parse : string -> (expr, string) result
